521c775f0eecd49f026234e6862a8d528a4b5206
Agrimagsrl <web@agrimag.it>
2020-11-08 16:29:30 +0100

    bump dist to 0.0.17

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ data = [filename.replace('eloquentarduino/', '')
 setup(
   name = 'eloquentarduino',
   packages = packages,
-  version = '0.0.15',
+  version = '0.0.17',
   license='MIT',
   description = 'A set of utilities to work with Arduino from Python and Jupyter Notebooks',
   author = 'Simone Salerno',
   author_email = 'eloquentarduino@gmail.com',
   url = 'https://github.com/eloquentarduino/eloquentarduino-python',
-  download_url = 'https://github.com/eloquentarduino/eloquentarduino-python/dist/eloquentarduino-0.0.15.tar.gz',
+  download_url = 'https://github.com/eloquentarduino/eloquentarduino-python/dist/eloquentarduino-0.0.17.tar.gz',
   keywords = [
     'ML',
     'Jupyter',
