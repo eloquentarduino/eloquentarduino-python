da7eb17ab3581ebb7763988b3adceff60d900d9d
eloquentarduino <eloquentarduino@gmail.com>
2020-12-08 19:03:38 +0100

    bump dist to 0.0.20

--- eloquentarduino/ml/data/__init__.py
+++ eloquentarduino/ml/data/__init__.py
@@ -3,3 +3,4 @@ from eloquentarduino.ml.data.processing import describe_data
 from eloquentarduino.ml.data.preprocessing import PrincipalFFT
 from eloquentarduino.ml.data.Pipeline import Pipeline
 from eloquentarduino.ml.data.PipelineGridSearch import PipelineGridSearch
+from eloquentarduino.ml.data.CheckpointFile import CheckpointFile
