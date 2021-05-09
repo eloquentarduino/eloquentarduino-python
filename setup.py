a39c6f2d519260ec18db7be4392a1e3ece330079
eloquentarduino <eloquentarduino@gmail.com>
2021-05-09 09:10:30 +0200

    bump dist to 0.0.55

--- setup.py
+++ setup.py
@@ -22,13 +22,13 @@ data = ["templates/on_device/sklearn/ResourcesBaseline.jinja", "templates/on_dev
 setup(
   name = 'eloquentarduino',
   packages = packages,
-  version = '0.0.54',
+  version = '0.0.55',
   license='MIT',
   description = 'A set of utilities to work with Arduino from Python and Jupyter Notebooks',
   author = 'Simone Salerno',
   author_email = 'eloquentarduino@gmail.com',
   url = 'https://github.com/eloquentarduino/eloquentarduino-python',
-  download_url = 'https://github.com/eloquentarduino/eloquentarduino-python/blob/master/dist/eloquentarduino-0.0.54.tar.gz?raw=true',
+  download_url = 'https://github.com/eloquentarduino/eloquentarduino-python/blob/master/dist/eloquentarduino-0.0.55.tar.gz?raw=true',
   keywords = [
     'ML',
     'Jupyter',
