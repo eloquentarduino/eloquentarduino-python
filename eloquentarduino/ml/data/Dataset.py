944fb780c4dd47664772a75e9c04e4c627942463
eloquentarduino <eloquentarduino@gmail.com>
2021-07-03 14:27:34 +0200

    better Dataset.plot

--- eloquentarduino/ml/data/Dataset.py
+++ eloquentarduino/ml/data/Dataset.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 from os.path import basename, splitext, sep
 from glob import glob
 from functools import reduce
@@ -115,7 +116,7 @@ class Dataset:
             valid_rows = slice(0, 999999)
 
         self.X = X[valid_rows]
-        self.y = np.asarray(y)[valid_rows]
+        self.y = np.asarray(y)[valid_rows].astype(np.int)
         self.columns = columns
         self.classmap = {-1: 'UNLABELLED'}
 
@@ -310,7 +311,7 @@ class Dataset:
 
         return arrays
 
-    def plot(self, title='', columns=None, n_ticks=15, grid=True, fontsize=6,  **kwargs):
+    def plot(self, title='', columns=None, n_ticks=15, grid=True, fontsize=6, bg_alpha=0.2, once_every=1, palette=None, **kwargs):
         """
         Plot dataframe
         :param title: str title of plot
@@ -318,9 +319,31 @@ class Dataset:
         :param n_ticks: int number of ticks on the x axis
         :param grid: bool wether to display the grid
         :param fontsize: int font size for the axis values
+        :param bg_alpha: float alpha of classes' background color
+        :param once_every: int limit the number of samples to draw
         """
         plt.figure()
-        self.df[columns or self.df.columns].plot(title=title, xticks=range(0, self.length, self.length // n_ticks), grid=grid, fontsize=fontsize, rot=70, **kwargs)
+        plot_columns = [c for c in (columns or self.df.columns) if c not in ['y', 'y_scale']]
+        df = pd.DataFrame(self.df[plot_columns].iloc[::once_every].to_numpy(), columns=columns or self.columns)
+        length = len(df)
+
+        df.plot(title=title, xticks=range(0, length, length // n_ticks), grid=grid, fontsize=fontsize, rot=70, **kwargs)
+
+        # highlight labels
+        y = self.y[::once_every]
+        loc_run_start = np.empty(len(y), dtype=bool)
+        loc_run_start[0] = True
+        np.not_equal(y[:-1], y[1:], out=loc_run_start[1:])
+        run_starts = np.nonzero(loc_run_start)[0]
+        run_lengths = np.diff(np.append(run_starts, len(y)))
+        run_values = y[loc_run_start]
+        palette = [c for c in (palette or mcolors.TABLEAU_COLORS.values())]
+
+        if self.y.max() >= len(palette):
+            print('[WARN] too many classes for the current palette')
+
+        for v, s, l in zip(run_values, run_starts, run_lengths):
+            plt.axvspan(s, s + l, color=palette[v], alpha=bg_alpha)
 
     def _get_label_id(self, label):
         """
