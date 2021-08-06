af7f3c941720fa3128cbd20eaf3f6801103fa59d
eloquentarduino <eloquentarduino@gmail.com>
2021-08-06 16:47:50 +0200

    fix XGBClassifier

--- eloquentarduino/ml/classification/sklearn/XGBClassifier.py
+++ eloquentarduino/ml/classification/sklearn/XGBClassifier.py
@@ -8,11 +8,59 @@ class XGBClassifier(SklearnClassifier, XGBImplementation):
     """
     xgboost.XGBClassifier wrapper
     """
-    def __init__(self, random_state=0, objective='multi:softprob', missing=None, **kwargs):
+    def __init__(self,
+                 max_depth=None,
+                 learning_rate=None,
+                 n_estimators=100,
+                 objective=None,
+                 gamma=None,
+                 min_child_weight=None,
+                 max_delta_step=None,
+                 subsample=None,
+                 colsample_bytree=None,
+                 colsample_bylevel=None,
+                 colsample_bynode=None,
+                 reg_alpha=None,
+                 reg_lambda=None,
+                 scale_pos_weight=None,
+                 base_score=None,
+                 random_state=None,
+                 missing=None,
+                 num_parallel_tree=None,
+                 monotone_constraints=None,
+                 interaction_constraints=None,
+                 importance_type="gain",
+                 gpu_id=None,
+                 validate_parameters=None,
+                 **kwargs):
         """
         Patch constructor
         """
-        super().__init__(random_state=random_state, objective=objective, missing=missing, **kwargs)
+        super(XGBClassifier, self).__init__(
+            max_depth=max_depth,
+            learning_rate=learning_rate,
+            n_estimators=n_estimators,
+            objective=objective,
+            gamma=gamma,
+            min_child_weight=min_child_weight,
+            max_delta_step=max_delta_step,
+            subsample=subsample,
+            colsample_bytree=colsample_bytree,
+            colsample_bylevel=colsample_bylevel,
+            colsample_bynode=colsample_bynode,
+            reg_alpha=reg_alpha,
+            reg_lambda=reg_lambda,
+            scale_pos_weight=scale_pos_weight,
+            base_score=base_score,
+            random_state=random_state,
+            missing=missing,
+            num_parallel_tree=num_parallel_tree,
+            monotone_constraints=monotone_constraints,
+            interaction_constraints=interaction_constraints,
+            importance_type=importance_type,
+            gpu_id=gpu_id,
+            validate_parameters=validate_parameters,
+            **kwargs)
 
     @property
     def sklearn_base(self):
@@ -20,9 +68,6 @@ class XGBClassifier(SklearnClassifier, XGBImplementation):
         Get xgboost implementation
         """
         return [base for base in self.__class__.__bases__ if base.__module__.startswith('xgboost.')][0]
-
-    def clone(self):
-        return XGBClassifier(**self.get_params())
 
     def fit(self, X, y):
         """
